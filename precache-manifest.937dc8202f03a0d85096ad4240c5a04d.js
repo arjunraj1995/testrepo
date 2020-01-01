62bf66125c8c54210f386ba314deee185d76d2a5
Arjun <arjun.cheirmarajan@tatacommunications.com>
2020-01-01 10:52:50 +0530

    Updates

---
+++ precache-manifest.937dc8202f03a0d85096ad4240c5a04d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de623da1305a261180f4aad81b6fe15b",
+    "url": "/testrepo/index.html"
+  },
+  {
+    "revision": "7a401a08c9932fca70ca",
+    "url": "/testrepo/static/css/main.91e47038.chunk.css"
+  },
+  {
+    "revision": "6224c58939339538d7d7",
+    "url": "/testrepo/static/js/2.c74b795d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/testrepo/static/js/2.c74b795d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a401a08c9932fca70ca",
+    "url": "/testrepo/static/js/main.e858a7ac.chunk.js"
+  },
+  {
+    "revision": "246a6720e2d378b84ae7",
+    "url": "/testrepo/static/js/runtime-main.61a2c48a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testrepo/static/media/logo.5d5d9eef.svg"
+  }
+]);
